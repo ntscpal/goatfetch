99e637d0888a47fd361b9d42a03da74e9ff4ed84
Marylin <ratgrrl@tuta.io>
2023-04-22 18:55:44 -0400

    Add comments

--- goatfetch.go
+++ goatfetch.go
@@ -9,39 +9,59 @@ import (
  )
 
 func main() {
+    // Get username
     logname := os.Getenv("LOGNAME")
+    // Get hostname
     hostnameFetch := exec.Command("hostname")
     hostnameOutput, _ := hostnameFetch.Output()
+    // Determine the number of dashes to seperate the username and hostname from the other statistics
     underNeed := len(logname) + len(hostnameOutput)
+    // Determine the user's shell
     shellPath := os.Getenv("SHELL")
+    // Only display the shell without its directory, e.g. 'bash' instead of '/bin/bash'
     shell := strings.TrimPrefix(shellPath, "/bin/")
+    // Determine user's terminal
     term := os.Getenv("TERM")
     prettyName := ""
+    // Fetch the kernel
     kernelFetch := exec.Command("uname", "-r")
     kernelOutput, _ := kernelFetch.Output()
-    file, _ := os.Open("/etc/os-release")
-    defer file.Close()
-    scanner := bufio.NewScanner(file)
+    // Determine the 'pretty name' defined in /etc/os-release
+    osReleaseFile, _ := os.Open("/etc/os-release")
+    defer osReleaseFile.Close()
+    scanner := bufio.NewScanner(osReleaseFile)
     for scanner.Scan() {
         line := scanner.Text()
         if strings.HasPrefix(line, "PRETTY_NAME=") {
             prettyName = strings.Trim(strings.TrimPrefix(line, "PRETTY_NAME="), `"`)
         }
     }
+    // Print the username
     fmt.Print("\033[32m\033[1m                   ",logname)
+    // Print atmark, 'user@hostname'
     fmt.Print("\033[0m@")
+    // Print the computer hostname
     fmt.Print("\033[32m\033[1m",string(hostnameOutput),"\033[0m")
+    // Blank spaces for formatting
     fmt.Print("                   ")
+    // Print dashes to seperate the 'user@hostname' from the system statistics
     for i := 0; i < underNeed; i++ {
         fmt.Print("-")
     }
-    fmt.Println("")
-    fmt.Println("  \033[1m(_(             ","\033[32mOS\033[0m:",prettyName)        
-    fmt.Print("  \033[1m/_/'_____/)      ","\033[32mKernel\033[0m: ",string(kernelOutput))
+    // Start new line
+    fmt.Println()
+    // Print the os name, or specifically the 'pretty name'
+    fmt.Println("  \033[1m(_(             ","\033[32mOS\033[0m:",prettyName)
+    // Print the kernel name        
+    fmt.Print("  \033[1m/_/'______/)     ","\033[32mKernel\033[0m: ",string(kernelOutput))
+    // Print the user's shell
     fmt.Println("  \033[1m\"  |      |     ","\033[32mShell\033[0m:",shell)
+    // Print the user's terminal
     fmt.Print("  \033[1m   |\"\"\"\"\"\"|      ","\033[32mTerminal\033[0m: ",term, "\n", "\n")
     
+    // Blank spaces for formatting
     fmt.Print("                   ")
+    // Print ANSI colours
     fmt.Print("\033[0;30m███")
     fmt.Print("\033[0;31m███")
     fmt.Print("\033[0;32m███")
@@ -50,8 +70,10 @@ func main() {
     fmt.Print("\033[0;35m███")
     fmt.Print("\033[0;36m███")
     fmt.Print("\033[0;37m███\n")
-    
+
+    // Blank spaces for formatting
     fmt.Print("                   ")
+    // Print brighter ANSI colours 
     fmt.Print("\033[0;90m███")
     fmt.Print("\033[0;91m███")
     fmt.Print("\033[0;92m███")
